2679596b80dd71439843b83826095db4d0a4f7e7
AlexPetrusca <alexandru.petrusca@yahoo.com>
2018-11-06 17:35:31 -0800

    make folders for each chromosome with jobs for each million base pair interval

--- vgtasker.py
+++ vgtasker.py
@@ -1,48 +1,52 @@
 import os
-import subprocess
-#https://stackoverflow.com/questions/13745648/running-bash-script-from-within-python
-shebang='#!/bin/bash'
-command = \
-    """#!/bin/bash
+import stat
+import shutil
 
-    java -Xmx15g -jar /hb/groups/corbettlab/DGN/gatk/gatk-package-4.0.10.1-local.jar GenomicsDBImport \\
-    --sample-name-map /hb/groups/corbettlab/DGN/sample_map.txt \\
-    --reader-threads 20 \\
-    --genomicsdb-workspace-path /hb/groups/corbettlab/DGN/{}/genomic_database_{} \\
-    --intervals {}:{}
-    """
+shebang = '#!/bin/bash'
+command = \
+"""
+#!/bin/bash
+
+java -Xmx15g -jar /hb/groups/corbettlab/DGN/gatk/gatk-package-4.0.10.1-local.jar GenomicsDBImport \\
+--sample-name-map /hb/groups/corbettlab/DGN/sample_map.txt \\
+--reader-threads 20 \\
+--genomicsdb-workspace-path /hb/groups/corbettlab/DGN/{}/genomic_database_{} \\
+--intervals {}:{}
+"""
 
 
 # to use docstring type in help(create_job_command)
-def create_job_command(filename, range,dirname):
-    '''
+def create_job_command(file_counter, chromosome, directory, range):
+    """
     DOCTSTRING
-    :param filename: chromosome name
+    :param chromosome: chromosome name
     :param range: steps of 1Mb starting from 0. I.e on first iteration it will be 0 up to 1Mb
     :return: the slurm job format
-    '''
-
-    filename2 = filename[0:9] + '_' + filename[-1]
-
-    slurm_job = command.format(filename2, range, filename, range)
-    filename = filename[0:11] + '.' + str(range) + '.' + filename[-2:]
-    with open(filename,'w') as out:
+    """
+    file_name = directory + '/' + str(file_counter) + ".sh"
+    slurm_job = command.format(directory, range, chromosome, range)
+    with open(file_name, 'w') as out:
         out.write(slurm_job)
+        st = os.stat(file_name)
+        os.chmod(file_name, st.st_mode | stat.S_IEXEC)
 
-        st = os.stat(filename)
-        os.chmod(filename, st.st_mode | 0o111)
-    os.system('mv filename {} '.format(dirname))
 
 for line in open('intervals.txt'):
     split = line.split()
     length = int(split[1])
     output_dir = split[:-1]
-    filename = split[0]+'.sh'
+    chromosome = split[0]
 
-    print(output_dir)
-    val = 0
-    while val != length:
-        newVal = min(val + 1000000, length)
-        range = '{}-{}'.format(val + 1, newVal)
-        val = newVal
-        create_job_command(filename, range,output_dir)
+    directory = chromosome[0:9] + '_' + chromosome[10]
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+    os.mkdir(directory)
+
+    lower_range = 0
+    file_counter = 1
+    while lower_range != length:
+        upper_range = min(lower_range + 1000000, length)
+        range = '{}-{}'.format(lower_range + 1, upper_range)
+        lower_range = upper_range
+        create_job_command(file_counter, chromosome, directory, range)
+        file_counter += 1
