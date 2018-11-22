631de68aa0aa7a4b4e577ef230e44713f78ba83c
Marcus Strauss <marcus@Animus.local>
2018-11-22 08:59:24 -0800

    bug fixes and changes

--- vcf_parser.py
+++ vcf_parser.py
@@ -1,3 +1,4 @@
+from itertools import dropwhile
 db={}
 class chromosomeValue:
     Rcount=Mcount=0
@@ -24,42 +25,43 @@ class chromosomeValue:
         :return:
         '''
         if c1 and c2:
+            print('case1',c1,c2,sep=',')
             self.Mcount+=2
-        if not c1 and not c2:
+        elif not c1 and not c2:
+            print('case2',c1, c2, sep=',')
             self.Rcount += 2
-        if c1 or c2:
+        else:
+            print('case3',c1, c2, sep=',')
             self.Rcount+=1
             self.Mcount+=1
     def total(self):
+        #print(self.Mcount,self.Rcount,sep=',')
         return 'Mutation rate = {}'.format(self.Mcount/(self.Rcount+self.Mcount))
 
 def main():
     with open('vcf.txt') as f:
-        for line in f:
-            if not line.startswith('#'):
-                split=line.split()
-                name, position=split[0], split[1]
+        for line in dropwhile(lambda x: x.startswith('#'),f):
+            split=line.split()
+            name, position=split[0], split[1]
+            key = (name, position)
 
-                if (name,position) not in db.keys():
-                    key=(name,position)
-                    value=chromosomeValue()
-                    db[key]=value
+            if key not in db.keys():
+                value=chromosomeValue()
+                db[key]=value
 
-                for sample in split[9:]:
-                    if len(sample.split('|'))<2:
-                        sample=sample.split('/')
-                    else:
-                        sample=sample.split('|')
-                    c1,c2=eval(sample[0]), eval(sample[1][0])
-                    db[key].find_count(c1, c2)
+            for sample in split[9:]:
+                if len(sample.split('|'))<2:
+                    sample=sample.split('/')
+                else:
+                    sample=sample.split('|')
+                c1,c2=eval(sample[0]), eval(sample[1][0])
+                db[key].find_count(c1, c2)
 
 if __name__=='__main__':
     main()
     for item in db.keys():
-        print(db[item])
-        print(db[item].total())
-
-
+        print(item,db[item].total(),sep=': ')
+        
 
 
 
