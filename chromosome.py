beecb05fcb96a6a4556302c43fc076a4fc7fb2d1
Marcus Strauss <marcus@eduroam-169-233-133-189.ucsc.edu>
2018-11-29 12:07:45 -0800

    put chromosome class in its own script and added cm args for vcf_parser

---
+++ chromosome.py
@@ -0,0 +1,33 @@
+class chromosomeValue:
+    Rcount = Mcount = 0
+    def __init__ (self):
+        self.Rcount = 0  #reference count
+        self.Mcount = 0  #Mutation count
+
+    def __str__(self):
+        return 'reference_count: {}, mutation_count: {}'.format(self.Rcount, self.Mcount)
+
+    def __iter__(self):
+        return iter(self.Rcount,self.Mcount)
+
+    def __getitem__(self, item):
+        return (self.Rcount,self.Mcount)[item]
+
+    def find_count(self,c1,c2):
+        '''
+        GT genotype, encoded as alleles values separated by
+        either of ”/” or “|”, e.g. The allele values are 0 for the reference allele (what is in the reference sequence),
+        1 for the first allele listed in ALT, 2 for the second allele list in ALT and so on.
+        :param c1: 0,1 or 2
+        :param c2: The count of a mutation or a reference.
+        :return:
+        '''
+        if c1 and c2:
+            self.Mcount+=1 #increment mutation count by 2
+        elif not c1 and not c2:
+            self.Rcount += 1 #increment reference count by 2
+        else:
+            self.Rcount+=1 #increment reference count by 1
+            self.Mcount+=1 #increment mutation count by 1
+    def total(self):
+        return self.Mcount/(self.Rcount+self.Mcount)
--- vcf_parser.py
+++ vcf_parser.py
@@ -1,63 +1,39 @@
 from itertools import dropwhile
-db={}
-cc_count=0
+from chromosome import chromosomeValue
+import argparse
+db={} #storage (chro. name, position): reference count, mutation count
+parser=argparse.ArgumentParser(description = 'Find the allele frequencies of '
+                                            'drosophilia flies from vcf files'
+                                             ' Example run: python3 vcf_parser.py .5')
 
-class chromosomeValue:
-    Rcount=Mcount=0
-    def init(self):
-        self.Rcount=0  #reference count
-        self.Mcount=0  #Mutation count
-
-    def __str__(self):
-        return 'reference_count: {}, mutation_count: {}'.format(self.Rcount, self.Mcount)
-
-    def __iter__(self):
-        return iter(self.Rcount,self.Mcount)
-
-    def __getitem__(self, item):
-        return (self.Rcount,self.Mcount)[item]
-
-    def find_count(self,c1,c2):
-        '''
-        GT genotype, encoded as alleles values separated by
-        either of ”/” or “|”, e.g. The allele values are 0 for the reference allele (what is in the reference sequence),
-        1 for the first allele listed in ALT, 2 for the second allele list in ALT and so on.
-        :param c1: 0,1 or 2
-        :param c2: The count of a mutation or a reference.
-        :return:
-        '''
-        if c1 and c2:
-            self.Mcount+=1 #increment mutation count by 2
-        elif not c1 and not c2:
-            self.Rcount += 1 #increment reference count by 2
-        else:
-            self.Rcount+=1 #increment reference count by 1
-            self.Mcount+=1 #increment mutation count by 1
-    def total(self):
-        return 'Mutation rate = {}'.format(self.Mcount/(self.Rcount+self.Mcount))
+parser.add_argument('-f', '--filter', type = float, metavar='', help = 'Cutoff to see if a variant is '
+                                                 'significant or not Please supply filter as a float'
+                                                   'for instance .5')
+args = parser.parse_args()
+if args.filter>1:
+    raise Exception('Filter should not exceed 1, the filter was {}'.format(args.filter))
 
 def main():
-
     with open('vcf.txt') as f:
         for line in dropwhile(lambda x: x.startswith('#'),f):
             split=line.split()
-            global cc_count
-            if split[2]=='.': cc_count+=1
-            name, position=split[0], split[1]
+            name, position = split[0], split[1]
             key = (name, position)
 
             if key not in db.keys():
                 value=chromosomeValue()
                 db[key]=value
 
-                print(cc_count)
             for sample in split[9:]:
                 if len(sample.split('|'))<2:
                     sample=sample.split('/')
                 else:
                     sample=sample.split('|')
                 c1,c2=eval(sample[0]), eval(sample[1][0])
-                db[key].find_count(c1, c2)
+                db[key].find_count(c1, c2) #accumulating the count of reference and mutation alleles
+
+            if db[key].total()<args.filter: #filtering step
+                del db[key]
 
 if __name__=='__main__':
     main()
