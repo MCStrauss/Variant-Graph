b5da282fd1af6f80e30f7e47bf29f526056b4826
Marcus Strauss <marcus@Animus.local>
2018-12-17 15:28:35 -0800

    also some changes to chromosome.py added class variable to keep track of total number of objects

--- chromosome.py
+++ chromosome.py
@@ -1,8 +1,9 @@
 class chromosomeValue:
-    Rcount = Mcount = 0
+    num_instances = 0 #keeps track of the number of instances of our class
     def __init__ (self):
         self.Rcount = 0  #reference count
         self.Mcount = 0  #Mutation count
+        self.__class__.num_instances += 1
 
     def __str__(self):
         return 'reference_count: {}, mutation_count: {}'.format(self.Rcount, self.Mcount)
@@ -13,7 +14,7 @@ class chromosomeValue:
     def __getitem__(self, item):
         return (self.Rcount,self.Mcount)[item]
 
-    def find_count(self,c1,c2):
+    def find_count(self, c1 ,c2):
         '''
         GT genotype, encoded as alleles values separated by
         either of ”/” or “|”, e.g. The allele values are 0 for the reference allele (what is in the reference sequence),
@@ -23,11 +24,11 @@ class chromosomeValue:
         :return:
         '''
         if c1 and c2:
-            self.Mcount+=1 #increment mutation count by 2
+            self.Mcount += 1 #increment mutation count by 2
         elif not c1 and not c2:
             self.Rcount += 1 #increment reference count by 2
         else:
-            self.Rcount+=1 #increment reference count by 1
-            self.Mcount+=1 #increment mutation count by 1
+            self.Rcount += 1 #increment reference count by 1
+            self.Mcount += 1 #increment mutation count by 1
     def total(self):
         return self.Mcount/(self.Rcount+self.Mcount)
