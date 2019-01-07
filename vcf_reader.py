f5d492d9a466071551becd443becdce9a0394723
Marcus Strauss <marcus@Animus.local>
2019-01-06 22:57:39 -0800

    update on vcf_reader trying to fix inconsistency between our global allel frequency and pyvcf

--- vcf_reader.py
+++ vcf_reader.py
@@ -1,5 +1,5 @@
 import vcf,  vcf.filters
-from collections import deque
+from collections import deque, defaultdict
 import argparse
 from frequency import Frequency
 #parser = argparse.ArgumentParser(description = 'filter for VCF file')
@@ -12,22 +12,76 @@ alt_freq = {} #holds sample: namedtuple Allfreq
 history  = deque(maxlen = 2)
 
 def main():
+    check = 0
     DP = AD = 0
     for record in vcf_reader:
+        key = (record.CHROM, record.POS)
+        alt_freq[key] = {}
         for call in record.samples:
+
             if call.sample[0].isdigit():
                 sample = call.sample[:2]
             else:
                 sample = call.sample[0]
-
             history.append(sample)
 
             if sample != history[0]:
-                try:
-                    if sample not in alt_freq:
-                        alt_freq[sample] = Frequency(DP, AD, (DP - AD) / DP)
+                DP = AD = 0
+            check += 1
+            try:
+                DP += call['DP']
+                AD += int(call['AD'][0])
+                #print(key, sample, DP, AD)
+                if DP and AD:
+                    if sample not in alt_freq[key]:
+                        alt_freq[key][sample] = Frequency(DP, AD, (DP - AD) / DP)
                     else:
-                        alt_freq[sample].update(DP, AD)
+                        alt_freq[key][sample].update(DP, AD)
+                        #print(key, sample, alt_freq[key][sample].DP, alt_freq[key][sample].AD)
+            except TypeError: #avoids getting edgecase where DP = None and thus adding int + Nonetype throws error
+                DP = AD = 0
+            #if check>8:
+             #   break
+
+
+def check():
+    vcf_reader = vcf.Reader(open('vcf.txt', 'r'))
+    with open('reference.txt', 'w') as check:
+        for record in vcf_reader:
+            check.write(f'{record.CHROM} {record.POS} {record.aaf}\n')
+
+    with open('our_data.txt', 'w') as data:
+        for chrom, samples in alt_freq.items():
+            DP = AD = 0
+            for val in samples.values():
+                DP += val.DP
+                AD += val.AD
+
+            data.write(f'{chrom} {(DP - AD) / DP}\n')
+
+
+
+if __name__ == '__main__':
+    main()
+    with open('output.txt', 'w') as out:
+        for key, value in alt_freq.items():
+            out.write(f'sample_name - {key}')
+            for val in value.items():
+                out.write(f'{val}'.center(10,' '))
+            out.write('\n')
+    check()
+
+
+
+'''
+ history.append(sample)
+
+            if sample != history[0]:
+                try:
+                    if sample not in alt_freq[(record.CHROM, record.POS)]:
+                        alt_freq[(record.CHROM, record.POS)][sample] = Frequency(DP, AD, (DP - AD) / DP)
+                    else:
+                        alt_freq[(record.CHROM, record.POS)][sample].update(DP, AD)
 
                 except ZeroDivisionError:
                     pass
@@ -35,17 +89,4 @@ def main():
 
                 DP = AD = 0
 
-            ifDP = call['DP']
-
-            if ifDP:
-                DP += ifDP
-                AD += int(call['AD'][0])
-
-
-if __name__ == '__main__':
-    main()
-    with open('output.txt', 'w') as out:
-        for key, value in alt_freq.items():
-            out.write(f'sample_name - {key}:  {value}\n')
-
-
+'''
